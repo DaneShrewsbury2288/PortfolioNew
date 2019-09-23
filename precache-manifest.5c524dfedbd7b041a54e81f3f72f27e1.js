c8359c3613b4bbe6d2d9284abe60ee3f8b90e233
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2019-09-23 15:04:41 -0700

    Updates

---
+++ precache-manifest.5c524dfedbd7b041a54e81f3f72f27e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea16b0cf9f883f4cac2",
+    "url": "/PortfolioNew/static/js/runtime~main.9ea16b0c.js"
+  },
+  {
+    "revision": "ee196a04d3daebe64e32",
+    "url": "/PortfolioNew/static/js/main.ee196a04.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/PortfolioNew/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee196a04d3daebe64e32",
+    "url": "/PortfolioNew/static/css/main.77ce52ca.chunk.css"
+  },
+  {
+    "revision": "f4acf74421b95888207ca1f6114f0042",
+    "url": "/PortfolioNew/index.html"
+  }
+];
